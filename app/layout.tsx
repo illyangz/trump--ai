a6c7d784587f3cf4a588490ca8f9f5efe93848cd
Hassan Mango <hmango77@gmail.com>
2025-01-07 16:36:43 -0600

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,31 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Trump's Artificial Intelligence",
-  description: "45th & 47th President of the United States. Builder of walls, maker of deals, and winner of elections. Making America Great Again.",
+  description:"45th & 47th President of the United States. Builder of walls, maker of deals, and winner of elections. Making America Great Again.",
   icons: {
-    icon: "/trump-ai.png", // Path to your app icon
-  },
-  openGraph: {
-    title: "Trump's Artificial Intelligence", // Optional, can repeat title
-    description: "45th & 47th President of the United States. Builder of walls, maker of deals, and winner of elections. Making America Great Again.", // Optional, can repeat description
-    images: [
-      {
-        url: "/trump-ai.png", // Image path in the public folder
-        width: 800,
-        height: 600,
-        alt: "Trump's AI Homepage Image", // Alt text for the image
-      }
-    ],
-    site_name: "Trump's Artificial Intelligence", // Optional, the website's name
-  },
-  twitter: {
-    card: "summary_large_image", // Twitter card type (large image)
-    title: "Trump's Artificial Intelligence", 
-    description: "45th & 47th President of the United States. Builder of walls, maker of deals, and winner of elections. Making America Great Again.",
-    images: ["/home.png"], // Same image for Twitter card
+    icon: "/trump-ai.png" // Update this path to where your trumpy.png is located
   }
+  
 }
-
 
 export default function RootLayout({
   children
